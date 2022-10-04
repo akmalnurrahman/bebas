c655a814bbdf5cd5d2c364155a3fbb78354db53d
akmalnurrahman <muhamad.akmal.tkom419@polban.ac.id>
2022-10-04 15:39:31 +0700

    contoh

---
+++ src/main/kotlin/Conditional.kt
@@ -0,0 +1,11 @@
+fun main ( ) {
+    val isSamboGuilty = true
+    val isFalse = false
+    if(isSamboGuilty) {
+        println("He will be jailed")
+    } else if (isFalse){
+        println("Something Wrong with this country")
+    } else{
+        println("........")
+    }
+}
